e723d4375f9ce4b073f18b75ec1dd9ec8a2bdf7a
AlejandroBusi <alejandro_busi@hotmail.com>
2021-06-30 01:01:05 -0300

    Comenzando

---
+++ Js/app.js
@@ -0,0 +1,11 @@
+function Ej1(params) {
+  alert("Para ver el ejercicio funcionando presiona F12 y ve a consola.")
+  let Arr = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre",]
+  for (let index = 0; index < Arr.length; index++) {
+    console.group(Arr[index]);
+    console.groupEnd;
+  }
+}
+function Ej2(params) {
+  
+}
